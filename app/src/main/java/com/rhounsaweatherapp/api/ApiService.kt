00a5d449f42a9d67784a95fbc795de96f99eeddc
rhounsa <rhounsa@supplay.fr>
2022-07-18 12:14:40 +0200

    implement of api and injection classes

---
+++ app/src/main/java/com/rhounsaweatherapp/api/ApiService.kt
@@ -0,0 +1,42 @@
+package com.rhounsaweatherapp.api
+
+import com.squareup.moshi.Moshi
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * Created by rhounsa on 17/07/2022.
+ */
+
+
+interface ApiService {
+    @GET("onecall")
+    suspend fun getForecast(
+        @Query("lat") lat: Double,
+        @Query("lon") lon: Double,
+        @Query("units") units: String = "metric",
+        @Query("exclude") exclude: String = "minutely",
+//        @Query("appid") appId: String = BuildConfig.OWM_API_KEY,
+        @Query("appid") appId: String = "f218129264ba2288384cd29b3839d9f3"
+    ): Result.Success
+}
+
+class Api(moshi: Moshi) {
+    private val retrofit:ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(ApiService::class.java)
+    }
+
+    suspend fun getForecast(lat: Double, lon: Double): Result {
+        return try {
+            retrofit.getForecast(lat, lon)
+        } catch (e: Exception) {
+            Result.Failure(e.toString())
+        }
+    }
+}
---
+++ app/src/main/java/com/rhounsaweatherapp/di/Modules.kt
@@ -0,0 +1,76 @@
+package com.rhounsaweatherapp.di
+
+import android.app.Application
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.rhounsaweatherapp.api.Api
+import com.rhounsaweatherapp.api.LocationManager
+import com.rhounsaweatherapp.db.WeatherAppDb
+import com.rhounsaweatherapp.db.dao.CityDao
+import com.rhounsaweatherapp.db.dao.WeatherDao
+import com.rhounsaweatherapp.repository.Repository
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+/**
+ * Created by rhounsa on 17/07/2022.
+ */
+
+@Module
+@InstallIn(SingletonComponent::class)
+class Modules {
+    @Provides
+    @Singleton
+    fun weatherAppDb(app: Application) = WeatherAppDb.getInstance(app)
+
+
+    @Provides
+    @Singleton
+    fun cityDao(db: WeatherAppDb) = db.cityDao
+
+    @Provides
+    @Singleton
+    fun weatherDao(db: WeatherAppDb) = db.weatherDao
+
+    @Provides
+    @Singleton
+    fun moshi(): Moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    @Provides
+    @Singleton
+    fun weatherApi(moshi: Moshi) = Api(moshi)
+
+
+
+    @Provides
+    @Singleton
+    fun weatherRepo(
+        weatherDao: WeatherDao,
+        cityDao: CityDao,
+        api: Api,
+        lm: LocationManager,
+        prefs: DataStore<Preferences>
+    ) =
+        Repository(weatherDao, cityDao, api, lm, prefs)
+
+    @Provides
+    @Singleton
+    fun locationProvider(app: Application): FusedLocationProviderClient =
+        LocationServices.getFusedLocationProviderClient(app)
+
+
+    @Provides
+    @Singleton
+    fun locationManager(
+        fusedLocationClient: FusedLocationProviderClient,
+    ) = LocationManager(fusedLocationClient)
+}
